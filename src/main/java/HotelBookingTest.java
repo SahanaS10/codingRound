9ea0f81d6dd66061e964b2a940a8ba7a0131cc70
Sahana <sahana.suresh@olacabs.com>
2018-08-07 20:32:57 +0530

    Fix for null pointer exception as web elements were not intialised

--- src/main/java/HotelBookingTest.java
+++ src/main/java/HotelBookingTest.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
@@ -24,6 +25,7 @@ public class HotelBookingTest {
 
     @Test
     public void shouldBeAbleToSearchForHotels() {
+    	PageFactory.initElements(driver, this);
         setDriverPath();
 
         driver.get("https://www.cleartrip.com/");
